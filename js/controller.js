10f20cd11063d1f9bd766380d987c6fee23b0c69
Diptesh Patel <Diptesh Patel>
2016-07-18 23:23:08 -0700

    Update controller file and fixed bugs

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,4 @@
 app.controller('myCtrl', function($scope, dealData) {
-
-    $scope.groups =[100,90,80,70,60,50,40,30,20,10];
     var root = 'http://www.cheapshark.com/api/1.0/deals';
     dealData.getDeals(root).then(function(response) {
         $scope.deal = response.data;
